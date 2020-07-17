0c7ec2ccee6004d2238abf6192ab756cd66a9c46
Dillion Megida <dillionmegida@gmail.com>
2020-07-17 02:30:23 +0100

    feat: add notification component

---
+++ src/components/notification/index.js
@@ -0,0 +1,50 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import styles from "./index.module.scss";
+
+const Notification = ({
+  show = false,
+  type = "error",
+  msg = "hello excuse me",
+  onClickCloseBtn,
+}) => {
+  const nStyles = {
+    error: {
+      style: styles["notification-error"],
+    },
+    success: {
+      style: styles["notification-success"],
+    },
+  };
+
+  const nStyle = nStyles[type] || nStyles.error;
+
+  return (
+    <div
+      className={`${styles["notification-wrapper"]} ${
+        show ? styles["notification-show"] : ""
+      }`}
+    >
+      <div className={`${styles.notification} ${nStyle.style} `}>
+        <div className={styles["notification-msg"]}>{msg}</div>
+        <button
+          onClick={onClickCloseBtn}
+          className={styles["notification-close-btn"]}
+          type="button"
+        >
+          <img src="/icons/close-icon.png" alt="Close" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+Notification.propTypes = {
+  show: PropTypes.bool.isRequired,
+  type: PropTypes.string.isRequired,
+  msg: PropTypes.string.isRequired,
+  onClickCloseBtn: PropTypes.string.isRequired,
+};
+
+export default Notification;
--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,63 +1,84 @@
+// eslint-disable-next-line import/no-duplicates
 import React from "react";
+// eslint-disable-next-line import/no-duplicates
+import { useState } from "react";
+
 import styles from "./index.module.scss";
 
 import Layout from "../../components/Layout";
-// import { useState } from "react";
+import Notification from "../../components/notification";
 
-// const [searchInput, setSearchInput] = useState("");
-// const [notification, setNotification] = useState({
-//   show: false,
-//   msg: null,
-//   type: null,
-// });
+const Home = () => {
+  const [searchInput, setSearchInput] = useState("");
+  const [notification, setNotification] = useState({
+    show: false,
+    msg: null,
+    type: null,
+  });
 
-// const updateSearchInputState = ({ target: { value } }) => {
-//   setSearchInput(value);
-// };
+  const { show, msg, type } = notification;
 
-// const onSubmitSearchForm = (e) => {
-//   e.preventDefault();
-//   if (searchInput === "") {
-//     setNotification({
-//       show: true,
-//       msg: "Search field should not be empty",
-//     });
-//   }
-// };
-const Home = () => (
-  <Layout>
-    <main className={styles.main}>
-      <div className={styles.container}>
-        <div className={styles["main-top"]}>
-          <img src="/icons/search-icon.svg" alt="" />
-          <h1>
-            Search more than <strong>56M </strong> users
-          </h1>
+  const onClickCloseNotificationBtn = () => setNotification(false);
+
+  const updateSearchInputState = ({ target: { value } }) => {
+    setSearchInput(value);
+  };
+
+  const onSubmitSearchForm = (e) => {
+    e.preventDefault();
+
+    if (searchInput === "") {
+      setNotification({
+        show: true,
+        msg: "Search field should not be empty",
+      });
+    }
+  };
+
+  return (
+    <Layout>
+      <Notification
+        type={type}
+        show={show}
+        msg={msg}
+        onClickCloseBtn={onClickCloseNotificationBtn}
+      />
+      <main className={styles.main}>
+        <div className={styles.container}>
+          <div className={styles["main-top"]}>
+            <img src="/icons/search-icon.svg" alt="" />
+            <h1>
+              Search more than <strong>56M </strong> users
+            </h1>
+          </div>
+          <div className={styles["search-form"]}>
+            <form onSubmit={onSubmitSearchForm}>
+              <div className={styles["input-group"]}>
+                <input
+                  onChange={updateSearchInputState}
+                  placeholder="Search users on GitHub"
+                />
+              </div>
+              <div className={styles["submit-btn"]}>
+                <input type="submit" value="Search" />
+              </div>
+            </form>
+          </div>
+          <section className={styles["search-tips-section"]}>
+            <h2>Search Tips</h2>
+            <ul>
+              <li>
+                <strong>user:[username]</strong>: to search user by username
+              </li>
+              <li>
+                <strong>fullname:[fullname]</strong>: to search user by fullname
+              </li>
+            </ul>
+          </section>
         </div>
-        <div className={styles["search-form"]}>
-          <form>
-            <div className={styles["input-group"]}>
-              <input placeholder="Search users on GitHub" />
-            </div>
-            <div className={styles["submit-btn"]}>
-              <input type="submit" value="Search" />
-            </div>
-          </form>
-        </div>
-        <section className={styles["search-tips-section"]}>
-          <h2>Search Tips</h2>
-          <ul>
-            <li>
-              <strong>user:[username]</strong>: to search user by username
-            </li>
-            <li>
-              <strong>fullname:[fullname]</strong>: to search user by fullname
-            </li>
-          </ul>
-        </section>
-      </div>
-    </main>
-  </Layout>
-);
+      </main>
+    </Layout>
+  );
+};
 
 export default Home;
