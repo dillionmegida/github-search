5c2918ae4e5b6fe349c2603ba080baf32dadad03
Dillion Megida <dillionmegida@gmail.com>
2020-07-17 03:45:28 +0100

    feat: add loading component

---
+++ src/components/Loading/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./index.module.scss";
+
+const Loading = () => (
+  <div className={styles.loading}>
+    <div />
+    <div />
+    <div />
+  </div>
+);
+
+export default Loading;
